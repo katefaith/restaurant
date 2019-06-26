371bc9054a7ce046e4e6a4ff84ccb8f13077984b
Kate <chelsea-92@mail.ru>
2019-06-26 23:03:40 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync");
+var svgstore = require("gulp-svgstore");
+var svgmin = require("gulp-svgmin");
+var rename = require("gulp-rename");
+
+gulp.task("style", function() {
+  gulp.src("sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer({browsers: [
+        "last 1 version",
+        "last 2 Chrome versions",
+        "last 2 Firefox versions",
+        "last 2 Opera versions",
+        "last 2 Edge versions"
+      ]})
+    ]))
+    .pipe(gulp.dest("css"))
+    .pipe(server.reload({stream: true}));
+});
+
+gulp.task("symbols", function() {
+  return gulp.src("img/**/*.svg")
+  .pipe(plumber())
+  .pipe(svgmin())
+  .pipe(gulp.dest("img"))
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+
+  .pipe(rename("symbols.svg"))
+  .pipe(gulp.dest("img"));
+});
+
+gulp.task("serve", ["style", "symbols"], function() {
+  server.init({
+    server: ".",
+    notify: false,
+    open: true,
+    ui: false
+  });
+
+  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("*.html").on("change", server.reload);
+});
